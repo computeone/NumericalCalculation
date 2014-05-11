250804cd90307f4a039313e6c9eba1f1476e04ab
niubaisui <1216936151@qq.com>
2014-05-11 12:52:37 +0800

    预处理共轭梯度算法完成。

--- MatrixIterator.cpp
+++ MatrixIterator.cpp
@@ -4,6 +4,58 @@
 
 using namespace std;
 
+double* MatrixMutl(double **a, double *b, int n){
+	double *c = new double[n];
+	for (int i = 0; i < n; i++){
+		double s = 0;
+		for (int j = 0; j < n; j++){
+			s = s + a[i][j] * b[j];
+		}
+		c[i] = s;
+	}
+	return c;
+}
+double* MatrixMutl(double *a, int n, double t){
+	double *c = new double[n];
+	for (int i = 0; i < n; i++){
+		c[i] = t*a[i];
+	}
+	return c;
+}
+double* MatrixMinus(double *a, double *b, int n){
+	double *c = new double[n];
+	for (int i = 0; i < n; i++){
+		c[i] = a[i] - b[i];
+	}
+	return c;
+}
+double* MatrixAdd(double *a, double *b, int n){
+	double *c = new double[n];
+	for (int i = 0; i < n; i++){
+		c[i] = a[i] + b[i];
+	}
+	return c;
+}
+double** MatrixTranspose(double **a, int n){
+	double **c = new double*[n];
+	for (int i = 0; i < n; i++){
+		c[i] = new double[n];
+	}
+
+	for (int i = 0; i < n; i++){
+		for (int j = 0; j < n; j++){
+			c[j][i] = a[i][j];
+		}
+	}
+	return c;
+}
+double Norm(double *a, int n){
+	double s = 0;
+	for (int i = 0; i < n; i++){
+		s = s + pow(a[i], 2);
+	}
+	return s;
+}
 double Norm(double *a, double *b, int n){
 	double s = 0;
 	for (int i = 0; i < n; i++){
@@ -146,6 +198,76 @@ double * SOR(double w,int n, double *x0, double** a, int N, double TOL){
 	}
 	cout << "迭代次数超过最大迭代次数！" << endl;
 }
+double *Gradient(int n, double *x0, double **a, double *b, double **c, int N, double TOL){
+	double *r = new double[n];
+	double *v = new double[n];
+	double *w = new double[n];
+	double *u = new double[n];
+	double *x = new double[n];
+
+	r = MatrixMinus(b, MatrixMutl(a, x0, n), n);
+	w = MatrixMutl(c, r, n);
+	v = MatrixMutl(MatrixTranspose(c, n), w, n);
+	
+	double d = 0;
+	for (int i = 0; i < n; i++){
+		d = d + pow(w[i], 2);
+		x[i] = x0[i];
+	}
+
+	int k = 1;
+
+	std::cout << "第0次迭代：" << endl;
+	for (int i = 0; i < n; i++){
+		std::cout << setprecision(10) << setiosflags(ios::showpoint) << x0[i] << "  ";
+	}
+	std::cout << endl;
+	/*
+	开始迭代
+	*/
+	while (k <= N){
+		if (Norm(v, n) < TOL){
+			return x;
+		}
+
+		u = MatrixMutl(a, v, n);
+		double t = 0;//t=tk
+		for (int i = 0; i < n; i++){
+			t = t + v[i] * u[i];
+		}
+		t = d / t;
+
+		x = MatrixAdd(x, MatrixMutl(v, n, t), n);//x=xk
+		r = MatrixMinus(r, MatrixMutl(u, n, t), n);//r=rk
+		w = MatrixMutl(c, r, n);//w=wk
+
+		double dd = 0;//dd=<wk,wk>
+		for (int i = 0; i < n; i++){
+			dd = dd + pow(w[i], 2);
+		}
+
+		if (fabs(dd) < TOL){
+			if (Norm(r, n) < TOL){
+				return x;
+			}
+		}
+		double s = dd / d;
+		v = MatrixAdd(MatrixMutl(MatrixTranspose(c, n), w, n), MatrixMutl(v, n, s), n);
+		d = dd;
+
+		std::cout << "第" << k << "次迭代：" << endl;
+		for (int i = 0; i < n; i++){
+			std::cout << setprecision(10) << setiosflags(ios::showpoint) << x[i] << "  ";
+		}
+		std::cout << endl;
+
+
+		k = k + 1;
+
+		
+	}
+	cout << "超过最大迭代次数！" << endl;
+}
 void testJacobi(){
 	double **a = new double*[5];
 	for (int i = 0; i < 5; i++){
@@ -256,4 +378,49 @@ void testSOR(){
 	x0[3] = 0;
 
 	double *x = SOR(1.25,5, x0, a, 10, 0.0001);
+}
+void testGradient(){
+	double **a = new double*[3];
+	double *b = new double[3];
+	double *x0 = new double[3];
+	double **c = new double*[3];
+	for (int i = 0; i < 3; i++){
+		a[i] = new double[3];
+		c[i] = new double[3];
+	}
+
+	a[0][0]=4;
+	a[0][1]=3;
+	a[0][2]=0;
+
+	a[1][0] = 3;
+	a[1][1] = 4;
+	a[1][2] = -1;
+
+	a[2][0] = 0;
+	a[2][1] = -1;
+	a[2][2] = 4;
+
+	b[0] = 24;
+	b[1] = 30;
+	b[2] = -24;
+
+	x0[0] = 0;
+	x0[1] = 0;
+	x0[2] = 0;
+
+	for (int i = 0; i < 3; i++){
+		for (int j = 0; j < 3; j++){
+			if (i == j){
+				c[i][j] = 1;
+			}
+			else{
+				c[i][j] = 0;
+			}
+		}
+	}
+
+	Gradient(3, x0, a, b, c, 10, 0.0000000001);
+
+
 }
--- MatrixIterator.h
+++ MatrixIterator.h
@@ -2,11 +2,25 @@
 
 using namespace std;
 
+/*
+求两个矩阵运算
+*/
 
+double*  MatrixMutl(double *a, double *b, int n);
+double** MatrixMutl(double **a, double **b, int n);
+double*  MatrixMutl(double **a, double *b, int n);
+double*  MatrixMutl(double *a, int n, double t);
+double*  MatrixAdd(double *a, double *b, int n);
+double** MatrixAdd(double **a, double **b, int n);
+double*  MatrixMinus(double *a,double *b,int n);
+double** MatrixMinus(double **a, double **b, int n);
+double** MatrixTranspose(double **a, int n);
+double*  MatrixTranspose(double *a, int n);
 /*
 求两个向量的范数
 */
-
+double Norm(double *a, int n);
+//a-b的范数
 double Norm(double *a, double *b, int n);
 /*
 Jacobi迭代法，实现求AX=b
@@ -28,3 +42,11 @@ SOR算法
 
 double* SOR(double w,int n, double *x0,double **a, int N, double TOL);
 void testSOR();
+
+/*
+预处理共轭梯度法
+*/
+
+
+double *Gradient(int n,double *x0,double **a,double *b,double *c,int N,double TOL);
+void testGradient();
